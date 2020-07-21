5da30238c5649e1f927d23b4501b4e3a47cc8719
Shivangi Singh <shivangisingh211@outlook.com>
2020-07-21 23:24:23 +0530

    commit changes

--- Cogni/Controllers/valuesController.cs
+++ Cogni/Controllers/valuesController.cs
@@ -15,7 +15,7 @@ namespace Cogni.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Kubernetes", "Demo" };
         }
 
        
